ef58592b5d78f09bc9ccd05d9dc946af39271e7d
Daniel Olowoniyi <olowoniyidaniel@gmail.com>
2026-07-05 09:39:51 +0100

    new changes

--- src/main/java/com/example/demo/rest/StudentRestController.java
+++ src/main/java/com/example/demo/rest/StudentRestController.java
@@ -49,34 +49,4 @@ public class StudentRestController {
 
         return theStudents.get(studentId);
     }
-
-//    Add an exception handler
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
-//        create a student error resp
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-
-//        return response entity
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-//    add another exception handler... to catch any exception (catch all)
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
-        //        create a student error resp
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-
-        //        return response entity
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
 }
